ced790835a1290119ce8e3a1800647157a22db45
Leigh Silverstein <leighsilverstein@Leighs-MacBook-Pro.local>
2017-03-12 17:41:49 -0400

    dist

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _selectors = require('./selectors');
+
+var _selectors2 = _interopRequireDefault(_selectors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _selectors2.default;
